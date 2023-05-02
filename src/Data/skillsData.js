f5d798dbdee24c697ca08129b1b88666780e0b1e
TomasSP <113086584+TomasSP1@users.noreply.github.com>
2023-05-02 07:06:59 +0300

    Add files via upload

---
+++ src/Data/skillsData.js
@@ -0,0 +1,74 @@
+import htmlIcon from '../images/html-1.svg';
+import cssIcon from '../images/css-3.svg';
+import javascriptIcon from '../images/logo-javascript.svg';
+import reactIcon from '../images/react-2.svg';
+import sassIcon from '../images/sass-1.svg';
+import bootstrapIcon from '../images/bootstrap-5-1.svg';
+import nodejsIcon from '../images/nodejs-icon.svg';
+import pythonIcon from '../images/python-5.svg';
+import mysqlIcon from '../images/mysql-6.svg';
+import firebaseIcon from '../images/firebase-1.svg';
+import mongodbIcon from '../images/mongodb-icon-1.svg';
+
+const frontSkills = [
+    {
+        image: htmlIcon,
+        name: "HTML",
+        subtitle: "Intermediate"
+    },
+    {
+        image: cssIcon,
+        name: "CSS",
+        subtitle: "Advanced"
+    },
+    {
+        image: javascriptIcon,
+        name: "JavaScript",
+        subtitle: "Intermediate"
+    },
+    {
+        image: reactIcon,
+        name: "React",
+        subtitle: "Intermediate"
+    },
+    {
+        image: sassIcon,
+        name: "Sass",
+        subtitle: "Intermediate"
+    },
+    {
+        image: bootstrapIcon,
+        name: "Bootstrap",
+        subtitle: "Intermediate"
+    },
+];
+
+const backSkills = [
+    {
+        image: nodejsIcon,
+        name: "Nodejs",
+        subtitle: "Basic"
+    },
+    {
+        image: pythonIcon,
+        name: "Python",
+        subtitle: "Basic"
+    },
+    {
+        image: mysqlIcon,
+        name: "MySQL",
+        subtitle: "Basic"
+    },
+    {
+        image: firebaseIcon,
+        name: "Firebase",
+        subtitle: "Basic"
+    },
+    {
+        image: mongodbIcon,
+        name: "Mongodb",
+        subtitle: "Basic"
+    },
+];
+
+export {frontSkills, backSkills}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,20 +1,8 @@
+import { frontSkills, backSkills } from "../Data/skillsData";
+
 import { Slide } from "react-awesome-reveal";
 
 import { RiDatabase2Line, RiCodeSSlashFill } from 'react-icons/ri';
-
-import html from '../images/html-1.svg';
-import css from '../images/css-3.svg';
-import javascript from '../images/logo-javascript.svg';
-import reactImg from '../images/react-2.svg';
-import sass from '../images/sass-1.svg';
-import bootstrap from '../images/bootstrap-5-1.svg';
-import nodejsIcon from '../images/nodejs-icon.svg';
-import pythonIcon from '../images/python-5.svg';
-import mysqlIcon from '../images/mysql-6.svg';
-import firebaseIcon from '../images/firebase-1.svg';
-import mongodbIcon from '../images/mongodb-icon-1.svg';
-
-
 
 const Skills = () => {
     return (
@@ -24,113 +12,48 @@ const Skills = () => {
             <span className="section_subtitle">My programming skills</span>
 
             <div className="skills_container container grid section_border">
-                {/* ==================== SKILLS 1 ==================== */}
+
+                {/* ==================== FRONTEND SKILLS ==================== */}
                 <Slide direction="left" triggerOnce="true" duration="2000">
                     <div className="skills_content">
                         <h3 className="skills_title">
-                            <RiCodeSSlashFill className="skills-title-logo"/> Frontend Developer
+                            <RiCodeSSlashFill className="skills-title-logo" /> Frontend Developer
                         </h3>
 
                         <div className="skills_info">
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={html} alt="skills image" />
-                                </div>
-                                <h3 className="skills_name">HTML</h3>
-                                <span className="skills_subtitle">Intermediate</span>
-                            </div>
 
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={css} alt="skills image" />
+                            {frontSkills.map((frontSkill, index) => (
+                                <div className="skills_data" key={index + 1}>
+                                    <div className="skills_img-container">
+                                        <img src={frontSkill.image} alt="skills image" />
+                                    </div>
+                                    <h3 className="skills_name">{frontSkill.name}</h3>
+                                    <span className="skills_subtitle">{frontSkill.subtitle}</span>
                                 </div>
-                                <h3 className="skills_name">CSS</h3>
-                                <span className="skills_subtitle">Advanced</span>
-                            </div>
+                            ))}
 
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={javascript} alt="skills image" />
-                                </div>
-                                <h3 className="skills_name">JavaScript</h3>
-                                <span className="skills_subtitle">Intermediate</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={reactImg} alt="skills image" />
-                                </div>
-                                <h3 className="skills_name">React</h3>
-                                <span className="skills_subtitle">Intermediate</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={sass} alt="skills image" />
-                                </div>
-                                <h3 className="skills_name">Sass</h3>
-                                <span className="skills_subtitle">Intermediate</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={bootstrap} alt="skills image" />
-                                </div>
-                                <h3 className="skills_name">Bootstrap</h3>
-                                <span className="skills_subtitle">Intermediate</span>
-                            </div>
                         </div>
                     </div>
                 </Slide>
 
-                {/* ==================== SKILLS 2 ==================== */}
+                {/* ==================== BACKEND SKILLS ==================== */}
                 <Slide direction="right" triggerOnce="true" duration="2000">
                     <div className="skills_content">
                         <h3 className="skills_title">
-                            <RiDatabase2Line className="skills-title-logo"/> Backend Developer
+                            <RiDatabase2Line className="skills-title-logo" /> Backend Developer
                         </h3>
 
                         <div className="skills_info">
 
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={nodejsIcon} alt="Nodejs icon" />
+                            {backSkills.map((backSkill, index) => (
+                                <div className="skills_data" key={index + 1}>
+                                    <div className="skills_img-container">
+                                        <img src={backSkill.image} alt="skills image" />
+                                    </div>
+                                    <h3 className="skills_name">{backSkill.name}</h3>
+                                    <span className="skills_subtitle">{backSkill.subtitle}</span>
                                 </div>
-                                <h3 className="skills_name">Nodejs</h3>
-                                <span className="skills_subtitle">Basic</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={pythonIcon} alt="Python icon" />
-                                </div>
-                                <h3 className="skills_name">Python</h3>
-                                <span className="skills_subtitle">Basic</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={mysqlIcon} alt="MySQL icon" />
-                                </div>
-                                <h3 className="skills_name">MySQL</h3>
-                                <span className="skills_subtitle">Basic</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={firebaseIcon} alt="Firebase icon" />
-                                </div>
-                                <h3 className="skills_name">Firebase</h3>
-                                <span className="skills_subtitle">Basic</span>
-                            </div>
-
-                            <div className="skills_data">
-                                <div className="skills_img-container">
-                                    <img src={mongodbIcon} alt="Mongodb icon" />
-                                </div>
-                                <h3 className="skills_name">Mongodb</h3>
-                                <span className="skills_subtitle">Basic</span>
-                            </div>
+                            ))}
 
                         </div>
                     </div>
